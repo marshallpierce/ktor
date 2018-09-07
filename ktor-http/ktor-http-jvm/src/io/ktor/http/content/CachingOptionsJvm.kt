bc0e21b27eccc3bf06b3ed3baf6120320477d29a
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-09-07 18:07:42 +0300

    Migrate ktor to new mpp plugin

--- ktor-http/ktor-http-jvm/src/io/ktor/http/content/CachingOptionsJvm.kt
+++
@@ -1,8 +0,0 @@
-package io.ktor.http.content
-
-import io.ktor.http.*
-import io.ktor.util.date.*
-import java.time.*
-
-fun CachingOptions(cacheControl: CacheControl? = null, expires: ZonedDateTime? = null) =
-        CachingOptions(cacheControl, expires?.toGMTDate())
--- ktor-http/src/io/ktor/http/ApplicationResponseProperties.kt
+++
@@ -1,5 +0,0 @@
-package io.ktor.http
-
-fun HeadersBuilder.contentType(contentType: ContentType) = set(HttpHeaders.ContentType, contentType.toString())
-fun HeadersBuilder.contentLength(length: Long) = set(HttpHeaders.ContentLength, length.toString())
-fun HeadersBuilder.etag(entityTag: String) = set(HttpHeaders.ETag, entityTag)
